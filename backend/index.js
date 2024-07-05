467c04848e43cbb7cc1300969d429711e0c326c3
halilkeklik <halil@halilkeklik.com>
2024-07-05 10:50:20 +0300

    Create authentication contorller

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,12 @@ const express = require("express")
 const connectDB =require("./database/db")
 const app = express()
 const dotenv=require("dotenv")
+const authRoute = require("./routes/auth")
 
 dotenv.config()
+app.use(express.json())
+app.use("/api/auth",authRoute)
+
 
 app.listen(process.env.PORT,()=>{
     connectDB()
