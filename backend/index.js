9e2cfdb6286e47699a6a478efe13669a48590045
halilkeklik <halil@halilkeklik.com>
2024-07-06 12:34:34 +0300

    Create post controller

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,8 @@ const connectDB =require("./database/db")
 const app = express()
 const dotenv=require("dotenv")
 const authRoute = require("./routes/auth")
-const userRoute=require("./routes/users")
+const userRoute=require("./routes/user")
+const postRoute=require("./routes/post")
 const cookieParser = require("cookie-parser")
 const path=require("path")
 const {errorHandler}=require("./middlewares/error")
@@ -16,6 +17,7 @@ app.use("/uploads",express.static(path.join(__dirname,"uploads")))
 
 app.use("/api/auth",authRoute)
 app.use("/api/user",verifyToken,userRoute)
+app.use("/api/post",verifyToken,postRoute)
 
 app.use(errorHandler)
 
